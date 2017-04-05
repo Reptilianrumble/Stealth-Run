96cdd4669074e983d98988d98105d4f8dfad33d6
JohnNolan85 <john.am.nolan85@gmail.com>
2017-04-06 00:14:30 +0100

    Update menu.lua

--- menu.lua
+++ menu.lua
@@ -94,3 +94,119 @@ scene:addEventListener( "destroy", scene )
 -- -----------------------------------------------------------------------------------
 
 return scene
+
+**********************************************
+******JOHN INSERTED AUDIO***********************
+**********************************************
+local composer = require( "composer" )
+
+local scene = composer.newScene()
+
+local soundTable = {
+
+	GadgetSound = audio.loadSound( "betaGadgetsound.wav" ),
+	DeathSound = audio.loadSound( "deathsound1.wav" ),
+	FootstepSound = audio.loadSound( "footstep.wav" ),
+	hitSound = audio.loadSound( "hitsound2.wav" ),
+	jumpSound = audio.loadSound( "jump.wav" ),
+	ladderSound = audio.loadSound( "ladder.wav" ),
+	menuSound = audio.loadSound( "menusound.wav" ), 
+	transitionSound = audio.loadSound( "transitionsound.wav" ),
+	victorySound = audio.loadSound( "victorysound3.wav" )
+	}
+
+-- -----------------------------------------------------------------------------------
+-- Code outside of the scene event functions below will only be executed ONCE unless
+-- the scene is removed entirely (not recycled) via "composer.removeScene()"
+-- -----------------------------------------------------------------------------------
+
+local function gotoGame()
+		audio.play (soundTable["transitionSound"])
+	composer.gotoScene ("level1", { time = 800, effect = "fade"} )
+end
+
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event functions
+-- -----------------------------------------------------------------------------------
+
+-- create()
+function scene:create( event )
+
+	local sceneGroup = self.view
+	-- Code here runs when the scene is first created but has not yet appeared on screen
+	
+	local background = display.newImageRect( sceneGroup, "testBackground.png", 3189, 3987 )
+    background.x = display.contentCenterX
+    background.y = display.contentCenterY
+	
+	local title = display.newImageRect( sceneGroup, "testTitles.png", 206, 118 )
+    title.x = display.contentCenterX
+    title.y = 200
+	
+	local playButton = display.newText( sceneGroup, "Start Game", display.contentCenterX, 233, native.systemFont, 64 )
+    playButton:setFillColor( 0.82, 0.86, 1 )
+ 
+    --local highScoresButton = display.newText( sceneGroup, "High Scores", display.contentCenterX, 810, native.systemFont, 44 )
+    --highScoresButton:setFillColor( 0.75, 0.78, 1 )
+	
+	playButton:addEventListener( "tap", gotoGame )
+    --highScoresButton:addEventListener( "tap", gotoHighScores )
+end
+
+
+
+-- show()
+function scene:show( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is still off screen (but is about to come on screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs when the scene is entirely on screen
+
+	end
+end
+
+
+-- hide()
+function scene:hide( event )
+
+	local sceneGroup = self.view
+	local phase = event.phase
+
+	if ( phase == "will" ) then
+		-- Code here runs when the scene is on screen (but is about to go off screen)
+
+	elseif ( phase == "did" ) then
+		-- Code here runs immediately after the scene goes entirely off screen
+
+	end
+end
+
+
+-- destroy()
+function scene:destroy( event )
+
+	local sceneGroup = self.view
+	-- Code here runs prior to the removal of scene's view
+
+end
+
+
+-- -----------------------------------------------------------------------------------
+-- Scene event function listeners
+-- -----------------------------------------------------------------------------------
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
+-- -----------------------------------------------------------------------------------
+
+return scene
+
+---------if you read this ily------
